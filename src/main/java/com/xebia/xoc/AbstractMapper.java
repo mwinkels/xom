2e27bcb95285d6ce0547b3945ad73786de12dea3
Maarten Winkels <mwinkels@xebia.com>
2012-08-27 03:47:25 +0200

    Extended Mapper interface to include mapping of list.

--- src/main/java/com/xebia/xoc/AbstractMapper.java
+++ src/main/java/com/xebia/xoc/AbstractMapper.java
@@ -1,17 +1,28 @@
 package com.xebia.xoc;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@SuppressWarnings("unchecked")
 public abstract class AbstractMapper implements Mapper {
   
-  @SuppressWarnings("unchecked")
   @Override
   public <S, T> T map(S source, Class<T> target) {
     return findClassMapper((Class<S>)source.getClass(), target).map(source);
   }
 
-  @SuppressWarnings("unchecked")
   @Override
   public <S, T> T map(S source, T target) {
     return findClassMapper((Class<S>)source.getClass(), (Class<T>)target.getClass()).map(source, target);
+  }
+  
+  @Override
+  public <S, T> List<T> map(List<S> source, Class<T> target) {
+    ArrayList<T> result = new ArrayList<T>(source.size());
+    for (S s : source) {
+      result.add(map(s, target));
+    }
+    return result;
   }
   
 }
--- src/main/java/com/xebia/xoc/Mapper.java
+++ src/main/java/com/xebia/xoc/Mapper.java
@@ -1,9 +1,13 @@
 package com.xebia.xoc;
 
+import java.util.List;
+
 public interface Mapper extends ClassMapperRegistry {
   
   <S, T> T map(S source, Class<T> target);
   
   <S, T> T map(S source, T target);
+  
+  <S, T> List<T> map(List<S> source, Class<T> target);
   
 }
