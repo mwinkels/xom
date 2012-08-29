057b66e11bde39d239f812aaa71fee5b52368887
Maarten Winkels <mwinkels@xebia.com>
2012-08-29 09:19:25 +0200

    Added additional test for array.

--- src/test/java/com/xebia/xoc/ArrayPropertySameTypeTest.java
+++ src/test/java/com/xebia/xoc/ArrayPropertySameTypeTest.java
@@ -1,14 +1,16 @@
 package com.xebia.xoc;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.sameInstance;
 import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
 import com.xebia.xoc.config.ClassMapperConfig;
 
-public class ArrayPropertyTest {
+public class ArrayPropertySameTypeTest {
   
   public static class Source {
     
@@ -25,13 +27,13 @@ public class ArrayPropertyTest {
   
   public static class Target {
     
-    private String[] b;
+    private Integer[] b;
     
-    public String[] getB() {
+    public Integer[] getB() {
       return b;
     } 
     
-    public void setB(String b[]) {
+    public void setB(Integer b[]) {
       this.b = b;
     }
     
@@ -46,7 +48,8 @@ public class ArrayPropertyTest {
     source.setA(new Integer[]{12,14});
     Target target = mapper.map(source);
     assertThat(target, is(notNullValue()));
-    assertThat(target.getB(), is(new String[]{"12", "14"}));
+    assertThat(target.getB(), is(source.getA()));
+    assertThat(target.getB(), is(not(sameInstance(source.getA()))));
   }
   
 }
